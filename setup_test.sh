a661e1759dd80da557e92307a064cfdba628a182
Pascal COMBES <pascom@orange.fr>
2020-05-27 09:22:27 +0200

    Licensing information.

--- setup_test.sh
+++ setup_test.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-# Copyright 2018 Pascal COMBES <pascom@orange.fr>
+# Copyright 2020 Pascal COMBES <pascom@orange.fr>
 #
-# This file is part of SolarProd.
+# This file is part of KeyboardCompositor.
 #
-# SolarProd is free software: you can redistribute it and/or modify
+# KeyboardCompositor is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# SolarProd is distributed in the hope that it will be useful,
+# KeyboardCompositor is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with SolarProd. If not, see <http://www.gnu.org/licenses/>
+# along with KeyboardCompositor. If not, see <http://www.gnu.org/licenses/>
 
 # Absolute path to folder containing script (and related files):
 SCRIPT_DIR="$(dirname "$0")"
